fc2673857f11b525b1bd3246417a7ab033763e7a
Tiancheng Xie <niconiconi@sjtu.edu.cn>
2018-11-19 11:17:38 -0800

    test

--- implementation/include/linear_gkr/verifier.h
+++ implementation/include/linear_gkr/verifier.h
@@ -11,7 +11,7 @@ class verifier
 public:
 	layered_circuit C;
 	prover *p;
-	void read_circuit();
+	void read_circuit(char *);
 	bool verify();
 	void get_prover(prover*);
 };
--- implementation/src/linear_gkr/verifier.cpp
+++ implementation/src/linear_gkr/verifier.cpp
@@ -4,3 +4,22 @@ void verifier::get_prover(prover *pp)
 {
 	p = pp;
 }
+
+void verifier::read_circuit(char *path)	
+{
+	int d;
+	FILE *circuit_in;
+	circuit_in = fopen(path, "r");
+
+	fscanf(circuit_in, "%d", &d);
+
+	for(int i = 0; i < d - 1; ++i)
+	{
+
+	}
+
+
+	fclose(circuit_in);
+	assert(false);
+	//TODO
+}
