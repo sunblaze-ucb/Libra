61eee16d1a8c8e400d05c815737bd5c4cffbdf43
Jiaheng <jiaheng_zhang@berkeley.edu>
2019-01-14 18:34:08 -0800

    sss

--- implementation/src/linear_gkr/main_zk.cpp
+++ implementation/src/linear_gkr/main_zk.cpp
@@ -3,16 +3,20 @@
 #include "linear_gkr/zk_verifier.h"
 #include "linear_gkr/zk_prover.h"
 #include "linear_gkr/prime_field.h"
-
+#include <iostream>
 zk_verifier v;
 zk_prover p;
 
 int main()
 {
+	std::cout << "hello world" << std::endl;
+
 	prime_field::init("16798108731015832284940804142231733909759579603404752749028378864165570215949", 10);
 	p.total_time = 0;
 	v.get_prover(&p);
+	std::cout << "come in" << std::endl;
 	v.read_circuit("test_circuit.txt");
+	std::cout << "after readfile" << std::endl;
 	p.get_circuit(v.C);
 	bool result = v.verify();
 	printf("%s\n", result ? "Pass" : "Fail");
