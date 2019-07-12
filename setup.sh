26fdf8600fbc3b2767c9b0c42907593788461fc8
Farmer <niconiconi@sjtu.edu.cn>
2019-07-11 21:51:23 -0700

    auto setup

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Script to setup environment
+sudo apt update
+sudo apt -y install make git clang++-7 libgmp-dev
+sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-7 1000
+git submodule init
+git submodule update
+make -C implementation/include/ate-pairing
