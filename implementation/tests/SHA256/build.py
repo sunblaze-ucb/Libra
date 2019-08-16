9dc3ec65aa2623f8f16cddaceb6915ce86c14074
Tiancheng Xie <niconiconi@sjtu.edu.cn>
2019-08-15 23:39:56 -0700

    stdc++11

--- implementation/tests/SHA256/build.py
+++ implementation/tests/SHA256/build.py
@@ -1,6 +1,6 @@
 import os
 os.system('./build.sh')
-os.system('g++ parser_sha_data_parallel.cpp -o psdp -O3')
+os.system('g++ -std=c++11 parser_sha_data_parallel.cpp -o psdp -O3')
 for i in range(8):
 	os.system('./psdp SHA256_64.pws SHA256_64_merkle_' + str(i + 1) + '_rdl.pws SHA256_64_merkle_' + str(i + 1) + '_circuit.txt SHA256_64_merkle_' + str(i + 1) + '_meta.txt')
 
